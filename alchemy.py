12482d508fe9ba94b62b1221c36bac78d970f10e
Gursimran Kaur <gursimrankaur196@gmail.com>
2016-10-14 15:02:17 +0530

    THIS IS FIRST COMMIT

---
+++ alchemy.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/ONE'
+db = SQLAlchemy(app)
+
+class def_repository(db.Model):
+	__tablename__ = 'def_repository'
+	id = db.Column('id' , db.Integer,primary_key=True)
+	name = db.Column('name' , db.String)
+	description = db.Column('description', db.String)
+	keyword = db.Column('keyword',db.String)
